533306746cf2acdd35e01c8dbddd2729960e986d
Maicon <Maicon.Queiroz.Contractor@pepsico.com>
2023-12-19 19:30:05 -0300

    subir alterações

--- src/academy/devdojo/maratonajava/introducao/OlaDevDojo.java
+++ src/academy/devdojo/maratonajava/introducao/OlaDevDojo.java
@@ -2,6 +2,6 @@ package academy.devdojo.maratonajava.introducao;
 
 public class OlaDevDojo {
     public static void main(String[] args){
-        System.out.println("KA-ME-HA-ME-HA");
+        System.out.println("Baseado no curso da DevDojo!!!");
     }
 }
