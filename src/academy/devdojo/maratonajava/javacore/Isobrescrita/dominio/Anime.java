0e5e247a1d0f171dd85e18e12fad519256990277
Maicon <Maicon.Queiroz.Contractor@pepsico.com>
2023-12-19 19:26:11 -0300

    starting class 76

---
+++ src/academy/devdojo/maratonajava/javacore/Isobrescrita/dominio/Anime.java
@@ -0,0 +1,9 @@
+package academy.devdojo.maratonajava.javacore.Isobrescrita.dominio;
+
+public class Anime {
+    private String nome;
+
+    public Anime(String nome) {
+        this.nome = nome;
+    }
+}
